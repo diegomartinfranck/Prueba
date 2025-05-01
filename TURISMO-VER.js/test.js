4141fb6ce7e38ec030dc47297b6088142fea7c31
diegomartinfranck <dmfranck@campus.ungs.edu.ar>
2025-05-01 02:56:47 -0300

    primera prueba

--- TURISMO-VER.js/test.js
+++ TURISMO-VER.js/test.js
@@ -1,1 +1,18 @@
 console.log("hola luuu");
+
+Diego = new Cliente ("Diego", "123456789");
+Martha = new Cliente ("Martha", "987654321");
+reserva = new Reserva;
+reserva.cliente = Diego;
+reserva.experienciaTuristica = new ExperienciaTuristica("Excursion a la montaña", 100, 10, "2023-10-01", "2023-10-05", 5);
+reserva.plazoLimite = "2023-10-01";
+reserva.cantidad = 5;           
+
+
+
+
+console.log(Diego());
+console.log(Diego.consultarDatos());
+console.log(Martha.consultarDatos());       
+console.log(Diego.telefono);
+console.log(Martha.telefono);
